1efb4b02ecee3428a117e44870e4a9393b957519
Rok Kralj <info@rok-kralj.net>
2014-12-29 08:12:03 +0100

    Ability to read piped files: cabal run < users.json

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
 module Main where
 
-	main = do
-		print "Let's get to work!"
+import Prelude hiding (print)
+
+import Text.Language.Json
+
+import Text.Syntax.Parser.Naive
+import Text.Syntax.Printer.Naive
+
+import Data.Maybe (fromJust)
+
+-- not a final version, handle failure
+handle :: String -> String
+handle s = fromJust $ print json $ head $ parse json s
+
+main :: IO ()
+main = interact handle
 
