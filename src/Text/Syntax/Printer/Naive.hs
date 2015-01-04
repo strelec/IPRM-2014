255b14fdaa39fb05ec77b7fd517e25ccbfd52431
Rok Kralj <info@rok-kralj.net>
2015-01-04 18:52:39 +0100

    Switch to ReaderT (Reader transformer on Maybe) monad.

--- src/Text/Syntax/Printer/Naive.hs
+++ src/Text/Syntax/Printer/Naive.hs
@@ -1,31 +1,31 @@
 module Text.Syntax.Printer.Naive where
 
-import Prelude (String, const)
+import Prelude (String, Maybe, const)
 
 import Control.Category ()
 import Control.Isomorphism.Partial (IsoFunctor ((<$>)), unapply)
-import Control.Monad (Monad, return, fail, (>=>), liftM2, mplus)
+import Control.Monad (Monad, return, fail, (>>=), liftM2, mplus)
+import Control.Monad.Reader (ReaderT, runReaderT, lift)
 
 import Data.Eq (Eq ((==)))
 import Data.Function (($))
 import Data.List ((++))
-import Data.Maybe (Maybe (Just, Nothing), maybe)
+import Data.Char (Char)
 
 import Text.Syntax.Classes (ProductFunctor ((<*>)), Alternative ((<|>), empty), Syntax (pure, token))
 
+
 -- printer
 
-newtype Printer alpha = Printer (alpha -> Maybe String)
+newtype Printer alpha = Printer (alpha -> ReaderT Char Maybe String)
 
 print :: Printer alpha -> alpha -> Maybe String
-print (Printer p) = p
+print (Printer p) s = runReaderT (p s) 'u'
 
-printM :: Monad m => Printer alpha -> alpha -> m String
-printM p x = maybe (fail "print error") return $ print p x
 
 instance IsoFunctor Printer where
   iso <$> Printer p
-    = Printer $ unapply iso >=> p
+    = Printer (\b -> lift (unapply iso b) >>= p)
 
 instance ProductFunctor Printer where
   Printer p <*> Printer q
@@ -34,10 +34,11 @@ instance ProductFunctor Printer where
 instance Alternative Printer where
   Printer p <|> Printer q
     = Printer (\s -> mplus (p s) (q s))
-  empty = Printer $ const Nothing
+
+  empty = Printer $ const $ fail "Empty."
 
 instance Syntax Printer where
-  pure x = Printer (\y ->  if x == y
-                             then Just ""
-                             else Nothing)
-  token = Printer (\t -> Just [t])
+  pure x = Printer (\y -> if x == y
+                          then return ""
+                          else fail "Pure constraint failed.")
+  token = Printer (\t -> return [t])
